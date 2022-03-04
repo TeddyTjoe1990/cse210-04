6947bcaaa8fd5daec5e91c0d25c09939df58cf9d
juliwhite <julietawhite30@gmail.com>
2022-03-04 15:45:11 -0700

    add gems and rock objects

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 
 
 import random
+from turtle import color
 
 from game.actor import Actor
 from game.artifact import Artifact
@@ -33,7 +34,7 @@ def main():
     cast = Cast()
     
     # create the banner
-    banner = Actor()
+    banner = Artifact()
     banner.set_font_size(FONT_SIZE)
     banner.set_color(WHITE)
     banner.set_position(Point(CELL_SIZE, 0))
@@ -50,11 +51,56 @@ def main():
     player.set_color(WHITE)
     player.set_position(position)
     cast.add_actor("robots", player)
+
+    #Create Gems.
+    list_gems = []
+    for i in range(DEFAULT_ARTIFACTS):
+        x = random.randint(0, COLS - 1)
+        y = random.randint(0, ROWS - 1)
+        position = Point(x, y)
+        position = position.scale(CELL_SIZE)
+
+        r = random.randint(0, 255)
+        g = random.randint(0, 255)
+        b = random.randint(0, 255)
+        color = Color(r, g, b)
+
+        gem = Artifact()
+        gem.set_text("*")
+        gem.set_font_size(FONT_SIZE)
+        gem.set_color(color)
+        gem.set_position(position)
+        list_gems.append(gem)
+        gem.set_lists(list_gems)
+        cast.add_actor("gems", gem)
+
+    # Create Rocks. 
+    list_rocks = []
+    for i in range(DEFAULT_ARTIFACTS):
+        x = random.randint(0, COLS - 1)
+        y = random.randint(0, ROWS - 1)
+        position = Point(x, y)
+        position = position.scale(CELL_SIZE)
+
+        r = random.randint(0, 255)
+        g = random.randint(0, 255)
+        b = random.randint(0, 255)
+        color = Color(r, g, b)
+
+        rock = Artifact()
+        rock.set_text("@")
+        rock.set_font_size(FONT_SIZE)
+        rock.set_color(color)
+        rock.set_position(position)
+        list_rocks.append(rock)
+        rock.set_lists(list_rocks)
+        cast.add_actor("rocks", rock)
    
     
     # start the game
     keyboard_service = KeyboardService(CELL_SIZE)
     video_service = VideoService(CAPTION, MAX_X, MAX_Y, CELL_SIZE, FRAME_RATE)
+    
     director = Game(keyboard_service, video_service)
     director.start_game(cast)
 
